3881ee9a10e9f37e7f1fba93b935aa92eb809f57
addmanrcace <addmanrcace@gmail.com>
2019-07-09 21:08:39 +0800

    Added Alix's Book List with functional API requests from Goodreads

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// const elements = document.querySelectorAll('.square');
+
+// anime({
+// 	targets: '.square',
+// 	translateX: 1000,
+// 	direction: 'alternate',
+// 	duration: 1000,
+// 	loop: true,
+// 	easing: 'easeInOutCirc'
+// });
+
+// anime({
+// 	targets: '.square-holder',
+// 	translateY: 500,
+// 	direction: 'alternate',
+// 	duration: 5000,
+// 	loop: true,
+// 	easing: 'linear'
+// });
