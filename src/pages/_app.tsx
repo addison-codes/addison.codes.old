9c08c6535bff3e395eb4ecbd0e564ac932b8bb7f
Addison Wanlass <hi@addison.codes>
2023-02-11 12:37:35 -0500

    feat: :zap: initialize project with homepage hero

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,31 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Space_Mono, Space_Grotesk } from '@next/font/google'
+const spaceMono = Space_Mono(
+  {
+  weight: '700',
+  subsets: ['latin']
+  }
+)
+const spaceGrotesk = Space_Grotesk(
+  {
+  weight: '700',
+  subsets: ['latin']
+  }
+)
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <style jsx global>
+    {`
+:root {
+  --spaceMono-font: ${spaceMono.style.fontFamily};
+  --spaceGrotesk-font: ${spaceGrotesk.style.fontFamily};
+}
+`}
+    </style>
+  <Component {...pageProps} />
+  </>
+  )
 }
